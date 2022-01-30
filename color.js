5625ff80cc8db8efa062712576d05de30c2e6890
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-01-30 08:13:28 +0100

    Completed half of the game

--- color.js
+++ color.js
@@ -5,3 +5,31 @@ for (var i = 10; i <= 20; i++) {
         console.log(i);
     }
 }
+
+var colors = [
+    "rgb(255,0,0)",
+    "rgb(255,255,0)",
+    "rgb(0,255,0)",
+    "rgb(0,255,255)",
+    "rgb(0,0,255)",
+    "rgb(255,0,255)"
+];
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+
+    squares[1].addEventListener("click", function () {
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            alert("correct!");
+        } else {
+            alert("WRONG!!!");
+        }
+    });
+}
