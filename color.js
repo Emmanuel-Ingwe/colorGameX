d0272190d4db44354eb4cc219a25b3a61bc5f2a3
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-02-09 19:59:00 +0100

    Fixed space error

--- color.js
+++ color.js
@@ -7,12 +7,12 @@ console.log("Connected");
 // }
 
 var colors = [
-    "rgb(255,0,0)",
-    "rgb(255,255,0)",
-    "rgb(0,255,0)",
-    "rgb(0,255,255)",
-    "rgb(0,0,255)",
-    "rgb(255,0,255)"
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
 ];
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -35,9 +35,10 @@ for (var i = 0; i < squares.length; i++) {
         }
     });
 }
+
 function changeColors(color) {
     for (var i = 0; i < squares.length; i++) {
-        squares(i).style.background = color;
+        squares[i].style.background = color;
     }
 }
 
