d8154054690a37005af377c6aa86665084ddf144
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-02-13 08:35:55 +0100

    Added styled.texts

--- color.js
+++ color.js
@@ -12,7 +12,17 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
+resetButton.addEventListener("click", function () {
+    colors = generateRandomcolors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+    }
+    h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,6 +34,7 @@ for (var i = 0; i < squares.length; i++) {
         console.log(clickedColor, pickedColor);
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!!";
+            resetButton.textContent = "play Again?";
             changeColors(clickedColor);
             h1.style.background = clickedColor;
         } else {
