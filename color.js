a7b41e890d209ae2629edd7083ba2e42c2814ddd
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-02-14 23:55:34 +0100

    Still addding

--- color.js
+++ color.js
@@ -50,6 +50,7 @@ resetButton.addEventListener("click", function () {
     colors = generateRandomcolors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
+    messageDisplay.textContent = "";
     for (var i = 0; i < squares.length; i++) {
         squares[i].style.background = colors[i];
     }
